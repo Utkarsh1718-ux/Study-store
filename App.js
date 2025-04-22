349e829fc4d42aa6506d7d1386573cf9b68a69cd
Utkarsh1718-ux <hurshit138@gmail.com>
2025-04-22 12:07:34 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card, CardContent } from "@mui/material";
+import { Button } from "@mui/material";
+
+const products = [
+  {
+    id: 1,
+    title: "NEET Biology Notes",
+    description: "Concise and complete handwritten notes for NEET aspirants.",
+    price: "₹199",
+  },
